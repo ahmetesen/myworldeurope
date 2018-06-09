95e85d737f18f0ffe0b95640e0810489e54faa32
Ahmet Esen <ahmetesen88@gmail.com>
2018-06-10 02:04:25 +0300

    servis json dataları oluşturuldu.

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,15 @@
 exports.index = function(req, res){
   res.render('index', { title: 'My World Europe' });
 };
+exports.save = function(req, res){
+  const fs = require('fs');
+
+  currentDate = new Date().toString() + " ";
+
+  currentDate+=req.body.log+"\r\n";
+  fs.appendFile('./public/history/log.txt', currentDate, function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+  
+};
