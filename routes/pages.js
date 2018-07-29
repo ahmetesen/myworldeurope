790c5c247269b1913540c5a2be2c371ba5265918
Ahmet Esen <ahmetesen88@gmail.com>
2018-07-29 18:10:07 +0300

    keywords added.

--- routes/pages.js
+++ routes/pages.js
@@ -7,27 +7,32 @@ var pageData = {
   index:{
     title:'My World Europe',
     description:'Ağaoğlu My World Europe Site Sakinleri - Bilgi Platformu',
-    canonical:'http://www.myworldeurope.net'
+    canonical:'http://www.myworldeurope.net',
+    keywords:'ağaoğlu, ağaoğlu my world europe, my world europe, myworldeurope'
   },
   servisler:{
     title:'Servis Saatleri - My World Europe',
     description:'Ağaoğlu My World Europe Sitesi\'nden Geçen Servislerin Saatleri',
-    canonical:'http://www.myworldeurope.net/servisler'
+    canonical:'http://www.myworldeurope.net/servisler',
+    keywords:'ağaoğlu, my world europe, servis, myworldeurope servis'
   },
   iett:{
     title:'İETT Saatleri - My World Europe',
     description:'Ağaoğlu My World Europe Sitesi\'nden Geçen Otobüslerin Saatleri',
-    canonical:'http://www.myworldeurope.net/iett'
+    canonical:'http://www.myworldeurope.net/iett',
+    keywords:'ağaoğlu, my world europe, iett, myworldeurope iett, otobüs, my world europe otobüs'
   },
   dukkanlar:{
     title:'Çarşı Dükkanları - My World Europe',
     description:'Ağaoğlu My World Europe Sitesi, Çarşı Alanı İrtibat Numaraları',
-    canonical:'http://www.myworldeurope.net/dukkanlar'
+    canonical:'http://www.myworldeurope.net/dukkanlar',
+    keywords:'ağaoğlu, my world europe, çarşı, dükkan, alış veriş'
   },
   iletisim:{
     title:'İletişim - My World Europe',
     description:'Ağaoğlu My World Europe Web Sitesi Yöneticilerine Ulaşın',
-    canonical:'http://www.myworldeurope.net/iletisim'
+    canonical:'http://www.myworldeurope.net/iletisim',
+    keywords:'ağaoğlu, my world europe, iletişim'
   }
 }
 
