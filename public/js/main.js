8bd1710d4c6b263bbce77e47d52e855bb6d06878
Ahmet Esen <ahmetesen88@gmail.com>
2018-08-09 20:05:48 +0300

    modal button "Kapat" hatası giderildi.

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,14 @@
 function showSimpleModal(title, message){
     $('.modal-title').text(title);
     $('.modalBody').text(message);
-    $('btn btn-secondary').text("Kapat!");
+    $('.btn.btn-secondary').html("Kapat!");
     $('.modal').modal();
 }
 
 function showTimeoutModal(title,message){
     $('.modal-title').text(title);
     $('.modalBody').text(message);
-    $('btn btn-secondary').text("Kapat!");
+    $('.btn.btn-secondary').html("Kapat!");
     $('.modal').modal();
     setTimeout(function(){
         $('.modal').modal('hide');
